7865ddcb09948d6ff81bc5f62d26856c2beeaeb1
arifkoyani <arifkoyani@gmail.com>
2025-05-07 00:09:47 +0200

    newcode

--- app.js
+++ app.js
@@ -3,22 +3,27 @@ const app = express();
 import cors from "cors";
 import RRouter from "./routes/user.route.js";
 import productsRoute from "./routes/product.route.js";
-// const WhiteListOrigins = ["http://localhost:5173"];
-// const corsOptions = {
-//   origin: function (origin, cb) {
-//     if (WhiteListOrigins.includes(origin) || !origin) {
-//       cb(null, true);
-//     } else {
-//       new throwerror("not allow");
-//     }
-//   },
-// };
+const whiteListOrigins = ["http://localhost:5173"];
+const corsOptions = {
+  origin: function (origin, cb) {
+    if (whiteListOrigins.includes(origin) || !origin) {
+      cb(null, true); // error success
+    } else {
+      cb(new Error("not allowed by cors"));
+    }
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PATCH", "DELETE"],
+};
 
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+// app.use(
+//   cors({
+//     origin: "*",
+//   })
+// );
+
+app.use(cors(corsOptions));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/", RRouter);
 app.use("/products", productsRoute);
